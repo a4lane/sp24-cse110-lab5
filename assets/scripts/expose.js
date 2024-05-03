93fd8450f402a46babbdae90882eba254854577f
a4lane <a4lane@ucsd.edu>
2024-05-03 12:12:45 -0700

    finished expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,74 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  
+  hornSelect.addEventListener("change", updateHorn);
+
+  button.addEventListener("click", playSound);
+
+  volumeSlider.addEventListener("change", updateVolume);
+
 }
+
+///// constant variables /////
+const image = document.querySelector("img");
+
+const audio = document.querySelector("audio");
+
+const hornSelect = document.getElementById("horn-select");
+
+const button = document.querySelector("button");
+
+const volumeSlider = document.getElementById("volume");
+
+const volumeIcon = document.getElementById("volume-controls").querySelector('img');
+
+const jsConfetti = new JSConfetti();
+
+// variable to hold current horn selection
+let currentHorn;
+
+
+// function called when a new horn is selected
+// loads new horns image and audio file
+function updateHorn(){
+  // get new horn selection
+  currentHorn = hornSelect.value;
+  // load correct image
+  image.src = "/assets/images/" + currentHorn + ".svg";
+  // load correct audio file
+  audio.src = "/assets/audio/" + currentHorn + ".mp3";
+
+}
+
+// function called when the button is clicked
+// plays correct audio file, adds confetti if currentHorn == party horn
+function playSound(){
+  if (currentHorn == "party-horn"){
+    jsConfetti.addConfetti();
+  }
+  audio.play();
+}
+
+
+function updateVolume(){
+  let currentVol = volumeSlider.value;
+  audio.volume = currentVol / 100;
+  if (currentVol == 0){
+    volumeIcon.src = "/assets/icons/volume-level-0.svg";
+  }
+  else if (currentVol < 33){
+    volumeIcon.src = "/assets/icons/volume-level-1.svg";
+  }
+  else if (currentVol < 67){
+    volumeIcon.src = "/assets/icons/volume-level-2.svg";
+  }
+  else {
+    volumeIcon.src = "/assets/icons/volume-level-3.svg";
+  }
+
+}
+
+
+
+
