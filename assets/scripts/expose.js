537959000742ac4e995e7e82a3478d7f8af0a35b
a4lane <146884160+a4lane@users.noreply.github.com>
2024-05-03 12:17:05 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -58,15 +58,19 @@ function updateVolume(){
   audio.volume = currentVol / 100;
   if (currentVol == 0){
     volumeIcon.src = "/assets/icons/volume-level-0.svg";
+    volumeIcont.alt = "Volume level 0";
   }
   else if (currentVol < 33){
     volumeIcon.src = "/assets/icons/volume-level-1.svg";
+    volumeIcont.alt = "Volume level 1";
   }
   else if (currentVol < 67){
     volumeIcon.src = "/assets/icons/volume-level-2.svg";
+    volumeIcont.alt = "Volume level 2";
   }
   else {
     volumeIcon.src = "/assets/icons/volume-level-3.svg";
+    volumeIcont.alt = "Volume level 3";
   }
 
 }
